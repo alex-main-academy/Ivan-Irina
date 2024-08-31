e73f8ab5c08b4d5c67f9302962563169e63b49fb
alex-main-academy <oleksandrilnytskyi@gmail.com>
2024-08-31 21:21:40 +0300

    add-fixed-hero

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-// var typed = new Typed("#title", {
-//     strings: ["Ivan &amp; <br> <span style='margin-left: 6rem'>Irina</span>"],
-//     typeSpeed: 150,
-//     backSpeed: 0,
-//     loop: false,
-//     showCursor: false,
-// });
+const heroBlocks = document.querySelectorAll(".js-hero-block");
+
+window.addEventListener("scroll", () => {
+    const cord = window.scrollY;
+
+    if (cord > window.innerHeight * 3) {
+        heroBlocks[0].classList.remove("active");
+        heroBlocks[1].classList.add("active");
+    } else {
+        heroBlocks[1].classList.remove("active");
+        heroBlocks[0].classList.add("active");
+    }
+});
