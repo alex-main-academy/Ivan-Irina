a5c1b945654a0e7413f55a0cf67762bfbfeab2c2
alex-main-academy <oleksandrilnytskyi@gmail.com>
2024-08-31 17:58:37 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var typed = new Typed("#title", {
+//     strings: ["Ivan &amp; <br> <span style='margin-left: 6rem'>Irina</span>"],
+//     typeSpeed: 150,
+//     backSpeed: 0,
+//     loop: false,
+//     showCursor: false,
+// });
